70576d18e93634c199af26e1ec1010c8d8e565e7
bitspusher <sanjay.mario@gmail.com>
2018-12-09 23:37:06 -0800

    Update tester.py
    Clean up unit testing using array of test arguments and expected results.

--- src/tester.py
+++ src/tester.py
@@ -13,22 +13,62 @@ from problem36 import *
 from problem37 import *
 from problem41 import *
 
+# The solved Project Euler problems that will be tested by this module
+eulerProbNumbers  = [12, 14, 15, 17, 18, 21, 23, 27, 30, 34, 35, 36, 37, 41]
+
+# Arguments to the problem-specific API. "None" if the API does not take arguments
+eulerProbArgument = [None,    # Prob. 12
+                     999999,  # Prob. 14
+                     21,      # Prob. 15
+                     1000,    # Prob. 17
+                     "../testFiles/prob18Data.txt", # Prob. 18
+                     10000,   # Prob. 21
+                     28123,   # Prob. 23
+                     None,    # Prob. 27
+                     5,       # Prob. 30
+                     None,    # Prob. 34
+                     1000000, # Prob. 35
+                     1000000, # Prob. 36
+                     None,    # Prob. 37
+                     None,    # Prob. 41
+                     "../testFiles/prob67Data.txt",    # Prob. 67
+                     
+# The expected result (return value) for a problem.
+sol = [76576500, # Prob. 12
+       837799,   # Prob. 14
+       137846528820, # Prob. 15
+       21124,   # Prob. 17
+       1074,    # Prob. 18
+       31626,   # Prob. 21
+       4179871, # Prob. 23
+      -59231,   # Prob. 27
+       443839,  # Prob. 30
+       40730,   # Prob. 34
+       55,      # Prob. 35
+       872187,  # Prob. 36
+       748317,  # Prob. 37
+       7652413, # Prob. 41
+       7273,    # Prob 67
+      ]
+      
+def runTests():
+    testIdx = 0
+    
+    for probNum in eulerProbNumbers:
+        funcName = "problem" + str(probNum)
+        if(eulerProbArgument[testIdx] == None):
+            assert globals()[funcName]() == sol[testIdx]
+        else:
+            assert globals()[funcName](eulerProbArgument[testIdx]) == sol[testIdx]
+        
+        print "problem" + str(probNum) + " test passed."
+        testIdx = testIdx + 1
+        
+    print "All tests passed successfully"
+
 def main():
     #Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers
-    #assert 4179871 == problem23(28123), "Prob. 23 test failed!"
-    #assert 76576500 == problem12(), "problem12 failed!"
-    #assert 837799 == problem14(999999), "Problem 14 test failed!" 
-    #assert 137846528820 == problem15(21), "prob. 15 test failed"
-    #assert 21124 == problem17(1000), "prob. 17 test failed" 
-    #assert 1074 == problem18("../testFiles/prob18Data.txt"), "prob. 18 test failed"
-    #assert 31626 == problem21(10000), "prob. 21 test failed!"
-    #assert -59231 == problem27(), "prob. 27 test failed"
-    #assert 443839 == problem30(5), "prob. 30 test failed"
-    #assert 40730 == problem34(), "prob. 34 test failed"
-    #assert 872187 == problem36(1000000), "prob. 36 failed!"
-    #assert 748317  == problem37(), "prob. 37 test failed"
-    #assert 7652413 == problem41(), "prob. 41 test failed"
-    #assert 7273 == problem18("../testFiles/prob67Data.txt"), "prob. 67 test failed"
-    assert 55 == problem35(1000000), "prob. 35 test failed"
+    runTests() 
+    
 if __name__ == "__main__":
     main()
